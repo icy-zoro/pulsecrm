563d60259a906d32f218d107428eacf694a2a0da
Alex Ivanov <oleksii120@gmail.com>
2024-08-14 11:42:34 +0300

    Changed title Added layout

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Header from './header/Header'
+import Main from './main/Main'
+import Sidebar from './sidebar/Sidebar'
+
+function App() {
+  return (<>
+    <Header />
+    <Sidebar />
+    <Main />
+  </>)
+}
+
+export default App
