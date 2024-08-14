c3754e76895a34cda1952c3a55e6b747e53e847f
Alex Ivanov <oleksii120@gmail.com>
2024-08-14 11:42:43 +0300

    Added header

---
+++ src/header/Header.tsx
@@ -0,0 +1,11 @@
+import './header.css'
+
+export default function Header() {
+    return (
+        <header>
+            <h1>
+                <a href="#">Pulse.crm</a>
+            </h1>
+        </header>
+    )
+}
